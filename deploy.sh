f7c21fd1651887736e54fd3a20df682f1603be20
Kele_Bingtang <2456019588@qq.com>
2021-12-15 00:32:35 +0800

    更新内容 + Action 自动部署

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ cd docs/.vuepress/dist
 # 如果发布到自定义域名，请使用
 # echo 'b.YoungKbt.com' > CNAME
 
-# 如果手运行该脚本，则执行if里的，如果是GitHub自动执行该脚本，则是else里的
+# 如果手运行该脚本，则执行 if 里的，如果是 GitHub 自动执行该脚本，则是 else 里的
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:Kele-Bingtang/notes-blog.git
 else
-  msg='来自github actions的自动部署'
+  msg='来自 Github actions 的自动部署'
   githubUrl=https://Kele-Bingtang:${GITHUB_TOKEN}@github.com/Kele-Bingtang/notes-blog.git
   git config --global user.name "Kele_Bingtang"
   git config --global user.email "2456019588@qq.com"
@@ -25,10 +25,10 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl master:gh-pages # 推送到 Github gh-pages 分支
 
 # deploy to coding pages
-# echo 'www.YoungKbt.com\YoungKbt.com' > CNAME  # 自定义域名
+# echo 'www.YoungKbt.cn\YoungKbt.cn' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
@@ -40,5 +40,7 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # git commit -m "${msg}"
 # git push -f $codingUrl master # 推送到coding
 
-cd - # 退回开始所在目录
+# 退回开始所在目录
+cd - 
+# 删除打包项目
 rm -rf docs/.vuepress/dist
