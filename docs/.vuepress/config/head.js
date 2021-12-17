a174112424a4208bdd3a2fc969db2fe96162669e
Kele_Bingtang <2456019588@qq.com>
2021-12-17 23:42:34 +0800

    更新内容 + Action 自动部署

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -10,20 +10,20 @@ module.exports = [
     },
   ],
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
-  
+
   ['script', { src: 'https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js' }],
   ['script', { src: 'https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js' }],
   ['script', { src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js' }],
   ['script', { src: 'https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js' }],
   // 百度统计 js
-  ['script', { src: '../common/baidu.js' }],
+  ['script', {}, 'var _hmt = _hmt || [];(function() {  var hm = document.createElement("script"); hm.src = "https://hm.baidu.com/hm.js?267c5680c2ffb468ca29c45ffe6801da"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();'],
 
- /*  [
-    'script',
-    {
-      'data-ad-client': 'ca-pub-7828333725993554',
-      async: 'async',
-      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-    },
-  ], */ // 网站关联Google AdSense 与 html格式广告支持
+  /*  [
+     'script',
+     {
+       'data-ad-client': 'ca-pub-7828333725993554',
+       async: 'async',
+       src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+     },
+   ], */ // 网站关联Google AdSense 与 html格式广告支持
 ]
