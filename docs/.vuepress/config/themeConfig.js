e4fa6f666f6398b109e3a06f47ff14722df658e8
Kele_Bingtang <2456019588@qq.com>
2022-01-21 20:21:54 +0800

    更新内容 + Action 自动部署

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -79,7 +79,7 @@ module.exports = {
     navColor: 2,    // 导航栏左侧名字、中间搜索框、右侧字体的颜色，1 是黑色，2 是白色。默认是 1
     switchNavColor: true,    // 页面移出大图片的位置后，navColor 是否变换，如由白色变黑色，黑色变白色。默认是 false
     // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
-    bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
+    bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
     bgTimeColorArray: ['transparent', 'transparent', 'transparent', 'transparent'],   // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效
     descFadeIn: true,   // 是否开启图片中间描述的淡入效果，默认为 false
     desc: ["故事由我书写，旅程由你见证，传奇由她聆听 —— 来自 Young Kbt", "Web前端技术博客，积跬步以至千里，致敬每个爱学习的你 —— 来自 Evan Xu", "这一生波澜壮阔或是不惊都没问题 —— 来自 Weibw"],  // 多条描述，如果填写则覆盖 index.md 的 tagline，不填写则默认读取 index.md 的 tagline，descFadeIn 为 true 生效
