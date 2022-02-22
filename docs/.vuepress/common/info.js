8b9921e95dec8329c3ef4800e30bb1623647e625
Kele_Bingtang <2456019588@qq.com>
2022-02-22 18:01:06 +0800

    添加项目工程文档

--- docs/.vuepress/common/info.js
+++ docs/.vuepress/common/info.js
@@ -2,7 +2,7 @@
 
 //注意：如果修改 penName 和 title，记得前往 docs/index 里第 4 行修改，尽量统一
 // 首页左上角 | 文章右上角的名字
-const penName = 'YoungKbt ';
+const penName = 'Young Kbt ';
 // 首页左上角的描述，衔接 penName
 const title = 'blog';
 // 文章右上角点击名字的跳转链接
--- docs/.vuepress/nav/moreNav.js
+++ docs/.vuepress/nav/moreNav.js
@@ -2,7 +2,7 @@ module.exports = {
   text: '更多',
   items: [
     { text: '轮子工具', link: '/whells-use/' },
-    { text: '项目过程', link: '/projects/' },
+    { text: '项目工程', link: '/projects/' },
     { text: '友情链接', link: '/friends/' }
   ]
 }
